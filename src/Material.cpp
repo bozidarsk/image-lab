4892920c1ca3ecdf7935d3958e3cee2b87dd7935
bozidarsk <bozidarsk@proton.me>
2026-04-25 15:55:28 +0300

    Default filters.

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,5 +1,73 @@
 #include "Material.h"
 
+/*static*/ const Material Material::Inverse = Material(Shader(&Shader::Inverse));
+/*static*/ const Material Material::Grayscale = Material(Shader(&Shader::Grayscale));
+/*static*/ const Material Material::ContrastStretch = Material(Shader(&Shader::ContrastStretch), { { "min", Color(0x00, 0x00, 0x00, 0xff) }, { "max", Color(0xff, 0xff, 0xff, 0xff) } });
+
+/*static*/ const Material Material::Blur = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00", 0.0625 }, { "m01", 0.125 }, { "m02", 0.0625 },
+		{ "m10", 0.125  }, { "m11", 0.25  }, { "m12", 0.125  },
+		{ "m20", 0.0625 }, { "m21", 0.125 }, { "m22", 0.0625 },
+	}
+);
+
+/*static*/ const Material Material::Sharpen = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00",  0.0 }, { "m01", -1.0 }, { "m02",  0.0 },
+		{ "m10", -1.0 }, { "m11",  5.0 }, { "m12", -1.0 },
+		{ "m20",  0.0 }, { "m21", -1.0 }, { "m22",  0.0 },
+	}
+);
+
+/*static*/ const Material Material::Emboss = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00", -2.0 }, { "m01", -1.0 }, { "m02",  0.0 },
+		{ "m10", -1.0 }, { "m11",  1.0 }, { "m12",  1.0 },
+		{ "m20",  0.0 }, { "m21",  1.0 }, { "m22",  2.0 },
+	}
+);
+
+/*static*/ const Material Material::Outline = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00", -1.0 }, { "m01", -1.0 }, { "m02", -1.0 },
+		{ "m10", -1.0 }, { "m11",  8.0 }, { "m12", -1.0 },
+		{ "m20", -1.0 }, { "m21", -1.0 }, { "m22", -1.0 },
+	}
+);
+
+/*static*/ const Material Material::TopSobel = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00",  1.0 }, { "m01",  2.0 }, { "m02",  1.0 },
+		{ "m10",  0.0 }, { "m11",  0.0 }, { "m12",  0.0 },
+		{ "m20", -1.0 }, { "m21", -2.0 }, { "m22", -1.0 },
+	}
+);
+
+/*static*/ const Material Material::BottomSobel = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00", -1.0 }, { "m01", -2.0 }, { "m02", -1.0 },
+		{ "m10",  0.0 }, { "m11",  0.0 }, { "m12",  0.0 },
+		{ "m20",  1.0 }, { "m21",  2.0 }, { "m22",  1.0 },
+	}
+);
+
+/*static*/ const Material Material::LeftSobel = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00",  1.0 }, { "m01",  0.0 }, { "m02", -1.0 },
+		{ "m10",  2.0 }, { "m11",  0.0 }, { "m12", -2.0 },
+		{ "m20",  1.0 }, { "m21",  0.0 }, { "m22", -1.0 },
+	}
+);
+
+/*static*/ const Material Material::RightSobel = Material(Shader(&Shader::ImageKernel),
+	{
+		{ "m00", -1.0 }, { "m01",  0.0 }, { "m02",  1.0 },
+		{ "m10", -2.0 }, { "m11",  0.0 }, { "m12",  2.0 },
+		{ "m20", -1.0 }, { "m21",  0.0 }, { "m22",  1.0 },
+	}
+);
+
 const Shader& Material::GetShader() const { return shader; }
 const Uniforms& Material::GetUniforms() const { return uniforms; }
 
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,6 +1,94 @@
+#include <algorithm>
 #include <cassert>
 
+#include "Image.h"
 #include "Shader.h"
+#include "Uniforms.h"
+
+/*static*/ Color Shader::Inverse(int x, int y, const Image& input, const Uniforms& uniforms) 
+{
+	auto color = input[x, y];
+	return Color(0xff - color.r, 0xff - color.g, 0xff - color.b, color.a);
+}
+
+/*static*/ Color Shader::Grayscale(int x, int y, const Image& input, const Uniforms& uniforms) 
+{
+	auto color = input[x, y];
+	auto luminance = (uint8_t)(255.0 * (0.2125*(color.r / 255.0) + 0.7154*(color.g / 255.0) + 0.0721*(color.b / 255.0)));
+
+	return Color(luminance, luminance, luminance, color.a);
+}
+
+/*static*/ Color Shader::ContrastStretch(int x, int y, const Image& input, const Uniforms& uniforms) 
+{
+	auto color = input[x, y];
+	auto min = uniforms.Get<Color>("min");
+	auto max = uniforms.Get<Color>("max");
+
+	auto r = (uint8_t)(255.0 * ((double)(color.r - min.r) / (double)(max.r - min.r)));
+	auto g = (uint8_t)(255.0 * ((double)(color.g - min.g) / (double)(max.g - min.g)));
+	auto b = (uint8_t)(255.0 * ((double)(color.b - min.b) / (double)(max.b - min.b)));
+
+	return Color(r, g, b, color.a);
+}
+
+/*static*/ Color Shader::ImageKernel(int x, int y, const Image& input, const Uniforms& uniforms) 
+{
+	if (x == 0 || y == 0 || x == input.GetWidth() - 1 || y == input.GetHeight() - 1)
+		return Color(0, 0, 0, input[x, y].a);
+
+	auto top_left = input[x - 1, y - 1];
+	auto top_center = input[x, y - 1];
+	auto top_right = input[x + 1, y - 1];
+
+	auto middle_left = input[x - 1, y];
+	auto middle_center = input[x, y];
+	auto middle_right = input[x + 1, y];
+
+	auto bottom_left = input[x - 1, y + 1];
+	auto bottom_center = input[x, y + 1];
+	auto bottom_right = input[x + 1, y + 1];
+
+	return Color(
+		(uint8_t)std::clamp(
+			top_left.r*uniforms.Get<double>("m00")
+			+ top_center.r*uniforms.Get<double>("m01")
+			+ top_right.r*uniforms.Get<double>("m02")
+			+ middle_left.r*uniforms.Get<double>("m10")
+			+ middle_center.r*uniforms.Get<double>("m11")
+			+ middle_right.r*uniforms.Get<double>("m12")
+			+ bottom_left.r*uniforms.Get<double>("m20")
+			+ bottom_center.r*uniforms.Get<double>("m21")
+			+ bottom_right.r*uniforms.Get<double>("m22"),
+			0.0, 255.0
+		),
+		(uint8_t)std::clamp(
+			top_left.g*uniforms.Get<double>("m00")
+			+ top_center.g*uniforms.Get<double>("m01")
+			+ top_right.g*uniforms.Get<double>("m02")
+			+ middle_left.g*uniforms.Get<double>("m10")
+			+ middle_center.g*uniforms.Get<double>("m11")
+			+ middle_right.g*uniforms.Get<double>("m12")
+			+ bottom_left.g*uniforms.Get<double>("m20")
+			+ bottom_center.g*uniforms.Get<double>("m21")
+			+ bottom_right.g*uniforms.Get<double>("m22"),
+			0.0, 255.0
+		),
+		(uint8_t)std::clamp(
+			top_left.b*uniforms.Get<double>("m00")
+			+ top_center.b*uniforms.Get<double>("m01")
+			+ top_right.b*uniforms.Get<double>("m02")
+			+ middle_left.b*uniforms.Get<double>("m10")
+			+ middle_center.b*uniforms.Get<double>("m11")
+			+ middle_right.b*uniforms.Get<double>("m12")
+			+ bottom_left.b*uniforms.Get<double>("m20")
+			+ bottom_center.b*uniforms.Get<double>("m21")
+			+ bottom_right.b*uniforms.Get<double>("m22"),
+			0.0, 255.0
+		),
+		middle_center.a
+	);
+}
 
 ShaderEntryPoint Shader::GetEntryPoint() const { return entryPoint; }
 
