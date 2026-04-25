70a51469a5419f9ec5297ff86aac15515170e84a
bozidarsk <bozidarsk@proton.me>
2026-04-25 22:57:37 +0300

    Default programs.

--- src/Program.h
+++ src/Program.h
@@ -14,10 +14,15 @@ private:
 	ProgramEntryPoint entryPoint;
 
 public:
+	static const Program PrintWorkingDirectory;
+	static const Program ChangeWorkingDirectory;
+
 	const std::string& GetName() const;
 	ProgramEntryPoint GetEntryPoint() const;
 
 	int Run(const ProgramArguments& args) const;
 
+	Program(const char* name, ProgramEntryPoint entryPoint);
 	Program(const std::string& name, ProgramEntryPoint entryPoint);
+	Program(std::string&& name, ProgramEntryPoint entryPoint);
 };
